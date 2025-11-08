04e1e05d88bfa5ca1cc784e702da437cacb08c55
Jihyeok Seo <ji@hyeok.org>
2025-11-09 08:40:45 +0900

    Bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "cafe.oeee"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
