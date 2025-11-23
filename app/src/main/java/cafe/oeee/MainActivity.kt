0c5bee57f67816ce097409347d863246c2703a6f
Jihyeok Seo <ji@hyeok.org>
2025-11-23 17:09:31 +0900

    sentry: Remove test error

--- app/src/main/java/cafe/oeee/MainActivity.kt
+++ app/src/main/java/cafe/oeee/MainActivity.kt
@@ -100,14 +100,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    // waiting for view to draw to better represent a captured error with a screenshot
-    findViewById<android.view.View>(android.R.id.content).viewTreeObserver.addOnGlobalLayoutListener {
-      try {
-        throw Exception("This app uses Sentry! :)")
-      } catch (e: Exception) {
-        Sentry.captureException(e)
-      }
-    }
 
         enableEdgeToEdge()
 
